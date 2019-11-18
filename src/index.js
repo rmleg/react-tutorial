7e1f3f204d1eef6d76b8ca4509dc2c2d1e789043
Rachel Leggett <rleggett@umich.edu>
2019-11-18 13:16:30 -0500

    second exercise done: multiple JSX elements in one functional component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<ul>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 3</li>
-</ul>, document.querySelector("#root"));
+function MyInfo () {
+    return (
+        <div class="hello">
+            <h1>Rachel Leggett</h1>
+            <p>Rachel is a web developer, 
+                a published knitwear designer, and a pole dancer living in Ann Arbor, MI.</p>
+            <ul>
+                <li>Italy</li>
+                <li>UK</li>
+                <li>Australia</li>
+            </ul>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <MyInfo />,
+    document.querySelector("#root")
+);
