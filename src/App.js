d1c5e28467eaa9470fa025a81769a710c122984e
Rachel Leggett <rleggett@umich.edu>
2019-11-19 15:39:03 -0500

    My solution to 'Mapping Components Practice'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+/* 
+Given a list of products (as an array of objects, as seen in productsData.js)
+render a <Product /> component (which you'll also need to create) for each
+product in the list.
+
+Make sure to use the array's `.map()` method to create these components, and 
+don't forget to pass a `key` prop to it to avoid the warning.
+*/
+
 import React from "react"
-import Joke from "./Joke"
-export default App
+import Product from './Product'
+import productsData from "./vschoolProducts"
 
 function App() {
+
+    const productsArray = productsData.map(
+        product => {
+            return (
+                <Product 
+                    key={product.id}
+                    name={product.name}
+                    price={product.price}
+                    description = {product.description}
+                />
+            )
+        }
+    )
+
     return (
         <div>
-            <Joke 
-                question="why did the chicken cross the road?"
-                answer="to get to the other side"
-            />
-            <Joke answer="That's what she said!" />
+            {productsArray}
         </div>
     )
 }
+
+export default App
