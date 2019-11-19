99a8a188fb1851d0932a0ab358c57c6d87e6b7bf
Rachel Leggett <rleggett@umich.edu>
2019-11-19 16:32:48 -0500

    class-based component practice

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,14 @@
 import React from "react"
 
-function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input type="checkbox" />
-            <p>{props.item.text}</p>
-        </div>
-    )
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <div className="todo-item">
+                <input type="checkbox" />
+                <p>{this.props.item.text}</p>
+            </div>
+        )
+    }
 }
 
 export default TodoItem
