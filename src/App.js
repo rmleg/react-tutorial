2e256503f29f8426e473a1542efbc8971035f6af
Rachel Leggett <rleggett@umich.edu>
2019-11-20 16:19:43 -0500

    State practice 2 and an onClick I added to update state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react"
 
-// Challenge:
-// Given an incomplete class-based component without a constructor, 
-// add a constructor and initialize state to fix the broken component.
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
 class App extends React.Component {
-
     constructor() {
         super();
         this.state = {
-            name: "Rachel",
-            age: 26,
+            isLoggedIn: true
         }
     }
 
+    updateState = () => {
+        this.setState({isLoggedIn: !this.state.isLoggedIn});
+    }
+
     render() {
-        return(
+        return (
             <div>
-                <h1>{this.state.name}</h1>
-                <h3>{this.state.age} years old</h3>
+                <h1 onClick={this.updateState}>You are currently logged {this.state.isLoggedIn ? 'in' : 'out'}.</h1>
             </div>
-        );
-    }    
+        )
+    }
 }
 
 export default App
