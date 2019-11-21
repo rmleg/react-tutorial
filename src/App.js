26dcf328f9a8b5dff09bb88009871fcb4d56b12e
Rachel Leggett <leggett.rachel@gmail.com>
2019-11-20 23:22:14 -0500

    Todo App Phase 6 solution. I should do this again because I need more practice.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
+/* *
+ * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
+ * This challenge is a little more involved than some of the past ones. Check the comments 
+ * in the code for some help on accomplishing this one
+ * 
+ * Challenge: 
+ * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
+ *    a. This method will be the trickest part. Check the comments in the stubbed-out
+ *  method below for some pseudocode to help guide you through this part
+ * 2. Pass the method down to the TodoItem component
+ * 3. In the TodoItem component, make it so when the `onChange` event happens,
+ *  it calls the `handleChange` method and passes the id of the todo into the function
+ */
+
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
 class App extends React.Component {
     constructor() {
-        super();
-        this.state = {
-            count: 0
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(){
-        /*this.setState({
-            count: this.state.count + 1
-        })*/
-        this.setState(
-            prevState => {
-                return {count: prevState.count + 1}
-            }
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.handleClick}>Change!</button>
-            </div>
-        )
-    }
-}
-
-/*class App extends React.Component {
-    constructor() {
         super()
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+        // Remember not to modify prevState directly, but instead to return a new version 
+        // of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+        this.setState((prevState) => {
+            return prevState.todos.map(item => {
+                if (item.id === id) {
+                    item.completed = !item.completed;
+                }
+            });
+        });
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        const todoItems = this.state.todos.map(
+                item => <TodoItem 
+                            onChange={(e) => this.handleChange(item.id, e)} 
+                            key={item.id} 
+                            item={item}
+                        />
+            )
         
         return (
             <div className="todo-list">
@@ -49,6 +53,6 @@ class App extends React.Component {
             </div>
         )    
     }
-}*/
+}
 
 export default App
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,40 @@
-/**
- * Challenge: Get rid of our warning about not having an onChange on our input. 
- * For now, the function that runs onChange can simply console.log something.
- */
-
 import React from "react"
 
-function TodoItem(props) {
+class TodoItem extends React.Component {
+    constructor() {
+        super();
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange() {
+        return this.props.onChange(this.props.item.id);
+    }
+
+    render() {
+        return (
+            <div className="todo-item">
+                <input 
+                    type="checkbox" 
+                    checked={this.props.item.completed} 
+                    onChange={this.onChange}
+                />
+                <p>{this.props.item.text}</p>
+            </div>
+        )
+    }
+}
+
+/* function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input onChange={() => console.log('changed')} type="checkbox" checked={props.item.completed}/>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={props.handleChange(props.item.id)}
+            />
             <p>{props.item.text}</p>
         </div>
     )
-}
+} */
 
 export default TodoItem
